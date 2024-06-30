92b5c604831ed511522055dec38640daa3640285
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2024-06-30 22:55:35 +0530

    Register page work started

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import './style/Register.css'
 
 function Register(){
     return(
-        <div>
-            <h1>Hi</h1>
+        <div className="register-grandparent">
+            <form className="register-parent">
+                <div className="user-parent">
+                    <label className="user-child" id="user-child-1">UserID</label>
+                    <input className="user-child"  id="user-child-2" type="text"/>
+                </div>
+
+                <div className="password-parent">
+                    <label>Password</label>
+                    <input type="password" />
+                </div>
+                <button className="submission">Submit</button>
+            </form>
         </div>
-    )
+    )   
 };
 
 export default Register;
